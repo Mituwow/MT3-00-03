c4876b6a7281c128512da6616023e06c0e961e44
Mituwow <114716282+Mituwow@users.noreply.github.com>
2024-04-23 12:34:58 +0900

    00-03課題完了。

--- MT3_00_03/main.cpp
+++ MT3_00_03/main.cpp
@@ -1,8 +1,102 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "GC1B_12_ハラサワミツタカ";
+const char kWindowTitle[] = "GC2A_09_ハラサワミツタカ";
 const int kWindowWidth = 1280;
 const int kWindowHeight = 720;
+
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	Matrix4x4 result;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (i == j) {
+				result.m[i][j] = 1;
+			}
+			if (i != j) {
+				result.m[i][j] = 0;
+			}
+		}
+	}
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	return result;
+}
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (i != j) {
+				result.m[i][j] = 0;
+			}
+		}
+	}
+	result.m[0][0] = scale.x;
+	result.m[1][1] = scale.y;
+	result.m[2][2] = scale.z;
+	result.m[3][3] = 1;
+	return result;
+}
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
+	Vector3 result;
+
+	result.x =
+		vector.x * matrix.m[0][0] +
+		vector.y * matrix.m[1][0] +
+		vector.z * matrix.m[2][0] +
+		1.0f * matrix.m[3][0];
+	result.y =
+		vector.x * matrix.m[0][1] +
+		vector.y * matrix.m[1][1] +
+		vector.z * matrix.m[2][1] +
+		1.0f * matrix.m[3][1];
+	result.z =
+		vector.x * matrix.m[0][2] +
+		vector.y * matrix.m[1][2] +
+		vector.z * matrix.m[2][2] +
+		1.0f * matrix.m[3][2];
+	float w =
+		vector.x * matrix.m[0][3] +
+		vector.y * matrix.m[1][3] +
+		vector.z * matrix.m[2][3] +
+		1.0f * matrix.m[3][3];
+
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+
+	return result;
+}
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
+	for (int row = 0; row < 4; row++)
+	{
+		for (int column = 0; column < 4; column++)
+		{
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + row * kRowHeight,
+				"%6.02f", matrix.m[row][column]
+			);
+		}
+	}
+}
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -12,8 +106,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	//宣言開始
+	Vector3 translate{ 4.1f,2.6f,0.8f };
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+
+	Vector3 scale{ 1.5f,5.2f,7.3f };
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+
+	Vector3 point{ 2.3f,3.8f,1.4f };
+	Matrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f
+	};
+
+	Vector3 transformed = Transform(point, transformMatrix);
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -35,6 +147,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+
+		Novice::ScreenPrintf(0, 0, "%6.02f", transformed.x);
+		Novice::ScreenPrintf(60, 0, "%6.02f", transformed.y);
+		Novice::ScreenPrintf(120, 0, "%6.02f", transformed.z);
+		MatrixScreenPrintf(0, kRowHeight * 2, translateMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 7, scaleMatrix);
 
 		///
 		/// ↑描画処理ここまで
